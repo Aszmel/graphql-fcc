3b47e443f0eadd8a412da47d40e1ea59f087ae1f
Aszmel <aszmel@o2.pl>
2018-11-24 18:43:21 +0100

    added client cra, apollo, cors, made book query js query with gql from apollo

--- server/app.js
+++ server/app.js
@@ -2,8 +2,12 @@ const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const schema = require("./schema/schema");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
+
+//allow cross-origin requests
+app.use(cors());
 
 //connect to mongo db
 mongoose.connect("mongodb://arek:sentencja1@ds115094.mlab.com:15094/gql-books");
